a1a9f34eadc0ca6adb7ef5ba0ce6393ae33eef3b
phuntshoWangdi <48612301+phuntshoWangdi@users.noreply.github.com>
2020-07-26 22:19:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from utils import stateOfBoard, locations
 import TicTacToeMaster as ttt
 import serial
 from time import sleep
+import cv2
 
 
-print("Please press enter to start...")
-cmd = input()
-
+sleep(1)
 #open port for communication with arduino
 try:
 	portToArduino = serial.Serial('COM9', 9600)
@@ -14,9 +13,15 @@ except:
 	print('Couldnot open the port')
 	exit()
 
+input("Please press enter to start...")
+
+prev_state_of_board = stateOfBoard()
+turn = 'comp'
+
 while 1:
 
 	cur_state_of_board = stateOfBoard()
+	
 	board, nextMove = ttt.getRandomBoard(cur_state_of_board)
 	ttt.printBoard(board)
 	
@@ -45,41 +50,68 @@ while 1:
 			position_to_send = locations[8]
 
 		cmd = position_to_send+'\n'
+		cmd_real = cmd
 
 		print(f'cmd: {cmd}')
 
-		cmd_temp = cmd[:]
-		cmd = cmd.encode('utf-8')
-		portToArduino.write(cmd)
-		sleep(1)
-		prev_cmd = cmd[:]
+		#
+		while (prev_state_of_board == cur_state_of_board) and turn == 'comp':
 
-		new_cmd = ''
-		for char in cmd_temp:
-			if char == 'z':
+			cmd_temp = cmd[:]
+			cmd = cmd.encode('utf-8')
+			portToArduino.write(cmd)
+			sleep(1)
+			prev_cmd = cmd[:]
+
+			new_cmd = ''
+			for char in cmd_temp:
+				if char == 'z':
+					break
+				new_cmd = new_cmd + char
+			
+			
+			#drop down endstop
+			new_cmd = new_cmd+'z-55\n'
+
+			print(f'new_cmd: {new_cmd}')
+
+			new_cmd = new_cmd.encode('utf-8')
+			portToArduino.write(new_cmd)
+			
+			#pull back endstop
+			new_cmd = prev_cmd[:]
+			portToArduino.write(new_cmd)
+			sleep(1)
+
+			#return back to home position
+			new_cmd = 'endstop\n'
+			new_cmd =  new_cmd.encode('utf-8')
+			portToArduino.write(new_cmd)
+
+			sleep(3)
+
+			cur_state_of_board = stateOfBoard()
+
+
+
+			if cur_state_of_board == prev_state_of_board:
+				cmd = cmd_real
+
+				
+			else:
+				prev_state_of_board = cur_state_of_board[:]
+				turn = 'human'
+				print("Next is human's turn.")
 				break
-			new_cmd = new_cmd + char
-		
-		
-		#drop down endstop
-		new_cmd = new_cmd+'z-55\n'
+	
 
-		print(f'new_cmd: {new_cmd}')
+		while prev_state_of_board == cur_state_of_board and turn == 'human':
+			
+			cur_state_of_board = stateOfBoard()
+			if prev_state_of_board != cur_state_of_board:
+				prev_state_of_board = cur_state_of_board[:]
+				turn = 'comp'
+				print("Next is comp's turn.")
+				break
 
-		new_cmd = new_cmd.encode('utf-8')
-		portToArduino.write(new_cmd)
-		
-		#pull back endstop
-		new_cmd = prev_cmd[:]
-		portToArduino.write(new_cmd)
-		sleep(1)
-
-		#return back to home position
-		new_cmd = 'endstop\n'
-		new_cmd =  new_cmd.encode('utf-8')
-		portToArduino.write(new_cmd)
-
-		cmd = input()
-		if cmd == 'e':
-			portToArduino.close()
-			break
+	
