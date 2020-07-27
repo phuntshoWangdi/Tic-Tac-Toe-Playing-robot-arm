95c39029636a760061c3345b671cd23038965c46
phuntshoWangdi <48612301+phuntshoWangdi@users.noreply.github.com>
2020-07-27 12:58:28 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,16 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2
+from time import sleep
 
 
-# locations = ['g1 x150 y118 z-40', 'g1 x132 y138 z-40',	'g1 x118 y15 z-40',
-# 			'g1 x128 y95 z-40',	  'g1 x105 y110 z-40',	'g1 x89 y125 z-40',
-# 			'g1 x95 y66 z-40',	  'g1 x79 y80 z-40',	'g1 x59 y89 z-40',
-# ]
-
-locations = ['g1 x157 y126 z-20', 'g1 x132 y138 z-20', 'g1 x111 y160 z-20',
-			'g1 x128 y95 z-20',   'g1 x105 y110 z-20', 'g1 x89 y125 z-20',
-			'g1 x98 y68 z-20',    'g1 x79 y80 z-20',   'g1 x68 y98 z-20'
+locations = ['g1 x157 y126 z-20', 'g1 x135 y145 z-20', 'g1 x116 y167 z-20',
+			'g1 x126 y99 z-20',   'g1 x102 y110 z-20', 'g1 x87 y131 z-20',
+			'g1 x97 y70 z-20',    'g1 x78 y84 z-20',   'g1 x62 y98 z-20'
 ]
-
-#last row -> 'g1 x95 y66 z-20 g1 x79 y80 z-20' 'g1 x59 y89 z-20'
-
 
 def stateOfBoard():
 	states = [' ']*9
-
 
 	pil_img = ImageGrab.grab(bbox=(275,292,683,730))
 	pil_img = pil_img.resize((408,408)).convert('L')
@@ -47,3 +39,25 @@ def stateOfBoard():
 		else: states[i] = ' '
 
 	return states
+
+def whosTurn():
+	sleep(1)
+	pil_img = ImageGrab.grab(bbox=(457,233,483,265)).convert('L') #bbox(x,y,w,h)
+
+	turn_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGB2BGR)
+
+
+
+	turn_o = cv2.imread('./template/turn_O.jpg')
+	turn_x = cv2.imread('./template/turn_X.jpg')
+
+	result_o = cv2.matchTemplate(
+								turn_img, 
+								turn_o, cv2.TM_CCOEFF_NORMED)
+	result_x = cv2.matchTemplate(
+								turn_img,
+								turn_x, cv2.TM_CCOEFF_NORMED) 
+
+	if np.amax(result_o) >= 0.8: return 'O'
+	elif np.amax(result_x) >= 0.8: return 'X'
+	else: return '-'
